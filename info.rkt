ce139ecf8152be3d978b5b3365441bfa9a1e5003
Leif Andersen <leif@leifandersen.net>
2015-10-09 09:01:28 -0400

    Initial commit.

---
+++ info.rkt
@@ -0,0 +1,10 @@
+#lang info
+(define collection "doodle")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/doodle.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(leif))
+(define raco-commands '(("doodle" (submod doodle main) "run Doodle" #f)))
---
+++ scribblings/doodle.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[doodle
+                    racket/base]]
+
+@title{doodle}
+@author{leif}
+
+@defmodule[doodle]
+
+Package Description Here
