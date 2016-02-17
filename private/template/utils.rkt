23a11aa65016558c998fc11b74f649409d2afa5d
Leif Andersen <leif@leifandersen.net>
2016-02-16 21:49:52 -0500

    Update template to include dot.

--- private/template/utils.rkt
+++ private/template/utils.rkt
@@ -1,7 +1,9 @@
 #lang racket
 
 (provide (all-defined-out))
-(require scribble/core)
+(require scribble/core
+         racket/draw
+         (only-in pict bitmap))
 
 (define (exact . items)
   (make-element (make-style "identity" '(exact-chars))
@@ -12,7 +14,18 @@
                 `("$" ,@items "$")))
 
 (define (mm . items)
-  (make-element (make-style "identify" '(exact-chars))
+  (make-element (make-style "identity" '(exact-chars))
                 `("\\[" ,@items "\\]")))
 
 (define at-char "@")
+
+(define dot (find-executable-path "dot"))
+
+(define (dot->pict . graph)
+  (define-values (in out) (make-pipe))
+  (with-input-from-string (apply string-append graph)
+    (λ () (parameterize ([current-output-port out])
+            (system* dot "-Tpng"))))
+  (define b (make-object bitmap% 1 1))
+  (send b load-file in)
+  (bitmap b))
